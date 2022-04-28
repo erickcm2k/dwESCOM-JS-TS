ade0418f5a3b3d29c56b1009344d79239a8a6338
luis.resendiz <luis.resendiz@ezetera.com.mx>
2022-04-27 20:30:35 -0500

    Clase 05 Strings

---
+++ 05 Strings/main.js
@@ -0,0 +1,82 @@
+// 1. charAt() // Nos permite acceder a un caracter de una cadena
+
+// console.log(s1.charAt(6)); // H
+
+// 2. includes() // Nos permite saber si una cadena contiene una subcadena
+
+// console.log(s1.includes(".")); // true
+// 3. indexOf() // Nos permite saber la posición de un subcadena en una cadena
+
+// console.log(s1.indexOf("s"));
+// 4. lastIndexOf() // Nos permite saber la posición de la ultima ocurrencia en una cadena
+// const x = "ntheiarhstdearhxstenarh.stearsti";
+// console.log(x.indexOf("."));
+// console.log(x.lastIndexOf("."));
+
+// const s1 = "perro";
+
+// 5. repeat() // Nos permite repetir una cadena
+
+// console.log(s1.repeat(8));
+
+// 6. replace() // Nos permite reemplazar una cadena por otra
+
+// const y = "hola como estas como estas";
+
+// console.log(y.replace("como", "perro"));
+
+// 7. replaceAll() // Nos permite reemplazar todas las ocurrencias de una cadena por otra
+
+// const y = "sssssssssssssssss";
+
+// console.log(y.replaceAll("s", "x"));
+
+// 8. slice() // Nos permite cortar una cadena
+
+// const str = "Hola mundo";
+
+// console.log(str.slice(-2));
+
+// const str = "Hola mundo";
+// console.log(str.slice(5, -2));
+
+// 9. split() // Nos permite dividir una cadena en un array
+
+// const str = "123,345,21342,234234,5436456,23423";
+
+// const array = str.split(",");
+// console.log(array);
+
+// 10. startsWith() // Nos permite saber si una cadena empieza con una subcadena
+
+// const x = "77234234234";
+
+// console.log(x.startsWith("55"));
+
+// 11. toLowerCase() // Nos permite pasar una cadena a minusculas
+
+// const x = "HNNNNNNNNNNNNNN";
+
+// console.log(x.toLowerCase());
+
+// 12. toUpperCase() // Nos permite pasar una cadena a mayusculas
+
+// const x = "eirstniersnit";
+
+// console.log(x.toUpperCase());
+
+// 13. trim() // Nos permite quitar espacios en blanco a los lados de una cadena
+
+// const x = "      Luis Fernando Resendiz      ";
+
+// console.log(x.trim());
+// console.log(x.replaceAll(" ", ""));
+
+// 14. trimStart() // Nos permite quitar espacios en blanco a los lados de una cadena
+
+// const x = "      Luis Fernando Resendiz      ";
+// console.log(x.trimStart());
+
+// // 15. trimEnd() // Nos permite quitar espacios en blanco a los lados de una cadena
+// const x = "      Luis Fernando Resendiz      ";
+// console.log(x.trimEnd());
